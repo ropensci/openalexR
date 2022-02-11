498b74065f1ae0d379288ff1117e8b3f65d82a06
massimoaria <aria@unina.it>
2022-02-11 12:25:51 +0100

    Several improvements Added the function oaList2df to convert an OpenAlex list into a data frame

---
+++ R/oaList2df.R
@@ -0,0 +1,177 @@
+utils::globalVariables("progress_bar")
+#' Convert OpenAlex data from a list to a data frame
+#' It converts bibliographic collection by OpenAlex database \href{https://openalex.org/}{https://openalex.org/} from a list to a data frame.
+#' The function converts a list obtained using \code{oaApiRequest} into a data frame/tibble. 
+#' 
+#' @param data is a list. data is the output of the function \code{oaApiRequest}. 
+#'
+#' @return a data.frame.
+#'
+#' For more extensive information about OpenAlex API, please visit: \href{https://docs.openalex.org/api}{https://docs.openalex.org/api}
+#'
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' 
+#' # Query to search all works citing the article:
+#' #  Aria, M., & Cuccurullo, C. (2017). bibliometrix:
+#' #   An R-tool for comprehensive science mapping analysis.
+#' #   Journal of informetrics, 11(4), 959-975.
+#'
+#' #  published in 2021.
+#' #  The paper is associated to the OpenAlex id W2755950973.
+#'
+#' #  Results have to be sorted by relevance score in a descending order.
+#'
+#' query <- oaQueryBuild(
+#' identifier=NULL,
+#' entity = "works",
+#' filter = "cites:W2755950973",
+#' date_from = "2021-01-01",
+#' date_to = "2021-12-31",
+#' search=NULL,
+#' sort="relevance_score:desc",
+#' endpoint = "https://api.openalex.org/")
+#'
+#' res <- oaApiRequest(
+#'    query_url = query,
+#'    format = "list",
+#'    total.count = FALSE,
+#'    verbose = FALSE
+#'    )
+#' 
+#' df <- oaList2df(res)   
+#' 
+#' df
+#'    
+#' }
+#' 
+#' @export
+oaList2df <- function(data){
+  
+  # replace NULL with NA
+  data <- simple_rapply(data, function(x) if(is.null(x)) NA else x)
+  
+  n <- length(data)
+  
+  list_df<- vector(mode = "list", length = n)
+  
+  pb <- progress::progress_bar$new(
+    format = "  data frame converting [:bar] :percent eta: :eta",
+    total = n, clear = FALSE, width = 60)
+  
+  
+  for (i in 1:n){
+    pb$tick()
+    print(i)
+    paper <- data[[i]]
+    
+    
+    id <- paper$id
+    title <- paper$display_name
+    pubdate <- paper$publication_date
+    relscore <- paper$relevance_score
+    # host venue
+    so_id <- paper$host_venue$id
+    so <- paper$host_venue$display_name
+    publisher <- paper$host_venue$publisher
+    issn <- list(unlist(paper$host_venue$issn))
+    url <- paper$host_venue$url
+    oa <- paper$host_venue$is_oa
+    
+    # authorships and affilitation
+    author <- list(do.call(rbind,lapply(paper$authorships, function(l){
+      
+      if (length(l[["institutions"]])>0){
+        institution_id <- l[["institutions"]][[1]]$id
+        institution_name <- l[["institutions"]][[1]]$display_name
+        institution_ror <- l[["institutions"]][[1]]$ror
+        institution_country <- l[["institutions"]][[1]]$country_code
+        institution_type <- l[["institutions"]][[1]]$type
+      } else {
+        institution_id <- NA
+        institution_name <- NA
+        institution_ror <- NA
+        institution_country <- NA
+        institution_type <- NA
+      }
+      L <- data.frame( 
+        au_id=l[["author"]]$id,
+        au_name=l[["author"]]$display_name,
+        au_orcid=l[["author"]]$orcid,
+        au_position=l$author_position,
+        au_affiliation_raw=l$raw_affiliation_string,
+          institution_id = institution_id,
+          institution_name = institution_name,
+          institution_ror = institution_ror,
+          institution_country = institution_country,
+          institution_type = institution_type
+      )
+    })))
+    
+    # concepts
+    concept <- list(do.call(rbind,lapply(paper$concepts, function(l){
+      L <- data.frame( 
+        concept_id=l$id,
+        concept_name=l$display_name,
+        concept_score=l$score,
+        concept_lecel=l$level,
+        concept_url=l$wikidata
+      )
+    })))
+    
+    TC <- paper$cited_by_count
+    PY <- paper$publication_year
+    cited_by_url <- paper$cited_by_api_url
+    ids <- unlist(paper$ids)
+    DI <- paper$doi
+    DT <- paper$type
+    CR <- unlist(paper$referenced_works)
+    related_works <- unlist(paper$related_works)
+    
+    # Abstract
+    if (!is.na(paper$abstract_inverted_index)){
+      ab <- data.frame(term=names(unlist(paper$abstract_inverted_index)),pos=unlist(paper$abstract_inverted_index))
+      
+      if (nrow(ab)>0){
+        ab$term2=gsub("[0-9]*$","",ab$term)
+        ### rimuove numeri al termine delle parole dell'abstract, e ne ordina i temrini
+        ab <- ab %>% 
+          group_by(.data$term2) %>% 
+          mutate(number = seq.int(length(.data$term2)),
+                 number = nchar(as.character(.data$number)),
+                 len = length(.data$term2))
+        ab2 <- ab %>% 
+          filter(.data$len==1)
+        
+        ab <- ab %>%
+          filter(.data$len>1) %>%
+          mutate(term = substr(.data$term,1,nchar(.data$term)-.data$number)) %>%
+          bind_rows(ab2) %>%
+          arrange(as.numeric(.data$pos))
+        ab <- paste(ab$term, collapse=" ")
+      } else {ab <- ""}
+    } else {ab <- ""}
+    
+    list_df[[i]] <- tibble(id=id, TI=title, author=author, AB=ab, pubdata=pubdate, 
+                           relscore=relscore, SO=so, SO_ID=so_id, PU=publisher, IS=issn, URL=url,
+                           OA=oa, TC=TC, PY=PY, cited_by_url=cited_by_url, 
+                           ids=list(ids), DI=DI, DT=DT, CR=list(CR), related_works=list(related_works),
+                           concept=concept)
+  }
+  df <- do.call(rbind,list_df)
+}
+
+
+# replace NULL with NA
+simple_rapply <- function(x, fn)
+{
+  if(is.list(x))
+  {
+    lapply(x, simple_rapply, fn)
+  } else
+  {
+    fn(x)
+  }
+}
--- R/oaQueryBuild.R
+++ R/oaQueryBuild.R
@@ -7,6 +7,8 @@
 #' @param filter is a character. Filters narrow the list down to just entities that meet a particular condition--specifically, a particular value for a particular attribute.
 #' Filters are formatted thusly: attribute:value. The complete list of filter attributes for each entity can be found
 #' at \href{https://docs.openalex.org/api/get-lists-of-entities#filter}{https://docs.openalex.org/api/get-lists-of-entities#filter}
+#' @param date_from is a character. It indicates the starting date of the time-span. The format is YYYY-MM-DD. The default values is \code{date_from=NULL}.
+#' @param date_to is a character. It indicates the ending date of the time-span. The format is YYYY-MM-DD. The default values is \code{date_from=NULL}.
 #' @param search is a character. Search is just another kind of filter, one that all five endpoints support. But unlike the other filters, search doesn't require an exact match.
 #' To filter using search, append .search to the end of the property you're filtering for.
 #' @param sort is character. Use the sort parameter to specify the property you want your list sorted by.
@@ -21,34 +23,56 @@
 #' @examples
 #'
 #' \dontrun{
+#' ### FIRST EXAMPLE
 #' # Query to search all works citing the article:
-#'   Aria, M., & Cuccurullo, C. (2017). bibliometrix:
-#'   An R-tool for comprehensive science mapping analysis.
-#'   Journal of informetrics, 11(4), 959-975.
+#' #  Aria, M., & Cuccurullo, C. (2017). bibliometrix:
+#' #   An R-tool for comprehensive science mapping analysis.
+#' #   Journal of informetrics, 11(4), 959-975.
 #'
-#'   published in 2021.
-#'   The paper is associated to the OpenAlex id W2755950973.
+#' #  published in 2021.
+#' #  The paper is associated to the OpenAlex id W2755950973.
 #'
-#'   Results have to be sorted by relevance score in a descending order.
+#' #  Results have to be sorted by relevance score in a descending order.
 #'
 #' query <- oaQueryBuild(
 #' identifier=NULL,
 #' entity = "works",
-#' filter="cites:W2755950973,publication_year:2021",
+#' filter = "cites:W2755950973",
+#' date_from = "2021-01-01",
+#' date_to = "2021-12-31",
 #' search=NULL,
 #' sort="relevance_score:desc",
 #' endpoint = "https://api.openalex.org/")
+#'
+#'
+#' ### SECOND EXAMPLE
+#' # Query to search all works containing the exact string
+#' # "bibliometric analysis" OR "science mapping" in the title, published in 2020 or 2021.
+#'
+#' # Results have to be sorted by relevance score in a descending order.
+#'
+#' query <- oaQueryBuild(
+#'    identifier=NULL,
+#'    entity = "works",
+#'    filter = 'title.search:"bibliometric analysis"|"science mapping"',
+#'    date_from = "2020-01-01",
+#'    date_to = "2021-12-31",
+#'    search=NULL,
+#'    sort="relevance_score:desc",
+#'    endpoint = "https://api.openalex.org/")
 #' }
+#'
 #'
 #'
 #' @export
 #'
 
-
 oaQueryBuild <- function(
   identifier = NULL, ## identifier of a work, author, venue, etc.
   entity = "works",
   filter=NULL,
+  date_from=NULL,
+  date_to=NULL,
   search=NULL,
   sort=NULL,
   endpoint = "https://api.openalex.org/") {
@@ -69,6 +93,10 @@ oaQueryBuild <- function(
 
   id <- c("NoMissing","Missing")
   id <- id[(is.null(identifier))+1]
+
+  if (!is.null(date_from)) date_from = paste(",from_publication_date:",date_from,sep="")
+  if (!is.null(date_to)) date_to = paste(",to_publication_date:",date_to,sep="")
+  filter <- paste(filter,date_from,date_to,sep="")
 
   switch(id,
          Missing={
