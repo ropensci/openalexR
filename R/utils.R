e80d8a6f060a16b708e402d460c7389520df9260
lelaboratoire <grixor@gmail.com>
2022-08-09 08:20:42 -0400

    update utils

--- R/utils.R
+++ R/utils.R
@@ -23,3 +23,20 @@ subs_na <- function(x, type = c("row_df", "col_df", "flat")) {
     flat = list(unlist(x))
   )
 }
+
+# append_query <- function(query_url) {
+#   ifelse(grepl("+[^?#]+\\?[^#:]+", query_url), "&", "?")
+# }
+#
+# filter_text <- function(query_url, cond = "per-page=1") {
+#   anchor <- append_query(query_url)
+#   paste0(query_url, anchor, cond)
+# }
+
+append_flt <- function(x, pre = "from_publication_date", collapse = "|"){
+  if (is.null(x)) return(NULL)
+  # x <- paste0("\"", x, "\"")
+  if (length(x) > 1) x <- paste(x, collapse = collapse)
+  paste0(pre, ":", x)
+}
+
