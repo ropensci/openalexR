b01c95646238831acdbee83a2530f41aeaaef45f
lelaboratoire <grixor@gmail.com>
2022-10-03 10:59:06 -0400

    edit test https://github.com/ourresearch/openalex-elastic-api/commit/150bf5b204cad174f09ab65f550174823f7e66a6

--- tests/testthat/test-oa_fetch.R
+++ tests/testthat/test-oa_fetch.R
@@ -1,9 +1,14 @@
 test_that("Invalid filter errors out", {
-  query_url <- "https://api.openalex.org/authors?filter=openalex%3AA923435168%7CA2208157607"
+  query_url <- "https://api.openalex.org/authors?filter=open_alex%3AA923435168%7CA2208157607"
 
   expect_error(oa_request(query_url))
 })
 
+test_that("oa_request returns list", {
+  query_url <- "https://api.openalex.org/authors?filter=openalex%3AA923435168%7CA2208157607"
+
+  expect_type(oa_request(query_url), "list")
+})
 
 test_that("oa_fetch works", {
   work_ids <- c("W2741809807")
