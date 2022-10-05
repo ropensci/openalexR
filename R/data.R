c4b6f44d0550fbb5413723ce914c8b5938a8ece1
lelaboratoire <grixor@gmail.com>
2022-10-05 17:44:20 -0400

    move countrycode

--- R/data.R
+++ R/data.R
@@ -7,3 +7,16 @@
 #' \code{id}, \code{display_name}, and \code{abbreviation}.
 #'
 "concept_abbrev"
+
+#' Index of Countries and their alpha-2 and aplha-3 codes.
+#'
+#' Data frame contains the list of countries and their alpha-2 and aplha-3 codes.\cr
+#'
+#' @format A data frame with 250 rows and 3 variables:
+#'  \describe{
+#'     \item{Country}{country names}
+#'     \item{Alpha2}{countries' alpha-2 codes}
+#'     \item{Alpha3}{countries' alpha-3 codes}
+#'     }
+#'
+"countrycode"
