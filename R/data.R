6416e2267ec52570e35fefd39769276a0b1db398
lelaboratoire <grixor@gmail.com>
2022-10-05 08:26:08 -0400

    add abbreviation reference

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' Concepts and abbreviations.
 #'
 #' 0-level concepts and corresponding abbreviations.
+#' Reference: https://www.ncbi.nlm.nih.gov/nlmcatalog/journals/
 #'
 #' @format A data frame with 19 observations and 3 variables:
 #' \code{id}, \code{display_name}, and \code{abbreviation}.
