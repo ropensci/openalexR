1e5e492ccaf38edccd6bb6496d1471b6fa38975c
lelaboratoire <grixor@gmail.com>
2022-11-08 08:40:54 -0500

    update test

--- tests/testthat/test-oa_query.R
+++ tests/testthat/test-oa_query.R
@@ -7,6 +7,14 @@ test_that("OA query works", {
         "10.1371/journal.pone.0267149"
       )
     ),
-    "https://api.openalex.org/works?filter=doi%3A10.1371%2Fjournal.pone.0266781%7C10.1371%2Fjournal.pone.0267149"
+    paste0(
+      "https://api.openalex.org/works?filter=doi%3A10.1371%2F",
+      "journal.pone.0266781%7C10.1371%2Fjournal.pone.0267149"
+    )
   )
+})
+
+test_that("oa_query returns NULL when no identifier or filter is supplied", {
+  expect_null(oa_query())
+  expect_message(oa_query())
 })
