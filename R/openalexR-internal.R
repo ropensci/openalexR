07002c444f6a99bb13fc5d0206126692357ff586
lelaboratoire <grixor@gmail.com>
2023-07-04 11:06:29 -0400

    message

---
+++ R/openalexR-internal.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname) {
+  message("Thank you for using openalexR!\n",
+    "To acknowledge our work, please cite the package by calling\n",
+    "`citation(\"openalexR\")`."
+  )
+}
