f77e77c158789a601158736281cdfe6cb35e2c38
lelaboratoire <grixor@gmail.com>
2024-11-15 11:22:38 -0500

    minor edit

--- R/openalexR-internal.R
+++ R/openalexR-internal.R
@@ -2,7 +2,9 @@
   if (!grepl("suppress", Sys.getenv("openalexR.message"), ignore.case = TRUE)){
     packageStartupMessage(
       "openalexR v2.0.0 introduces breaking changes.\n",
-      "See NEWS.md for details."
+      "See NEWS.md for details.\n\n",
+      "To suppress this message, add `openalexR.message = suppressed` ",
+      "to your .Renviron file."
     )
   }
 }
