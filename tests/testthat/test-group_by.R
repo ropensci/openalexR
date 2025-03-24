c5142bfd9ae579b53203c64f596624d4108c1fbf
Trang Le <grixor@gmail.com>
2025-03-24 13:34:43 -0400

    Fix group_by for boolean groups (#328)
    * closes #327

---
+++ tests/testthat/test-group_by.R
@@ -0,0 +1,22 @@
+
+test_that("search and group_by works", {
+  # https://github.com/ropensci/openalexR/issues/327
+  g1 <- oa_fetch(
+    title_and_abstract.search = "nature",
+    group_by = "topics.subfield.id"
+  )
+  expect_s3_class(g1, "data.frame")
+  expect_equal(colnames(g1), c("key", "key_display_name", "count"))
+
+})
+
+test_that("group_by works", {
+  # https://github.com/ropensci/openalexR/issues/327
+
+  # grouping by the boolean "is_oa" should return 2 rows
+  g2 <- oa_fetch(entity = "works", search = "biodiversity", group_by = "is_oa")
+  expect_s3_class(g2, "data.frame")
+  expect_equal(nrow(g2), 2)
+  expect_equal(colnames(g2), c("key", "key_display_name", "count"))
+})
+
